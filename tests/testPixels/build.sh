4038adff3dc572b9b3ad917e80c48a942a417aee
kalwalt <info@kalwaltart.it>
2024-04-24 23:04:43 +0200

    testing new PixelData class

---
+++ tests/testPixels/build.sh
@@ -0,0 +1,3 @@
+# you need to set the VIRGIL_PATH environment variable to the path of the Virgil folder
+
+v3c-x86-linux ${VIRGIL_PATH}/lib/util/Arrays.v3 ${VIRGIL_PATH}/lib/util/Ranges.v3 ../../src/Pixels.v3 testPixels.v3
