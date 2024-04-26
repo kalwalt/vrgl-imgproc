1360c7d3a7409cec1a41be16637b4c474f323817
kalwalt <info@kalwaltart.it>
2024-04-27 00:45:26 +0200

    improved testPixels and testing new grayscaleRGBA function

--- tests/testFilters/build.sh
+++ tests/testFilters/build.sh
@@ -1,5 +1,5 @@
 # you need to set the VIRGIL_PATH environment variable to the path of the Virgil folder
 
-v3c -target=wasm -heap-size=500m -shadow-stack-size=64m ${VIRGIL_PATH}/lib/util/Vector.v3 ${VIRGIL_PATH}/lib/util/Arrays.v3 ${VIRGIL_PATH}/lib/util/Ranges.v3 ../../src/Filters.v3 FiltersWasm.v3
+v3c -target=wasm -heap-size=500m -shadow-stack-size=64m ${VIRGIL_PATH}/lib/util/Vector.v3 ${VIRGIL_PATH}/lib/util/Arrays.v3 ${VIRGIL_PATH}/lib/util/Ranges.v3 ../../src/Pixels.v3 ../../src/Filters.v3 FiltersWasm.v3
 
-v3c-x86-linux ${VIRGIL_PATH}/lib/util/Vector.v3 ${VIRGIL_PATH}/lib/util/Arrays.v3 ${VIRGIL_PATH}/lib/util/Ranges.v3 ../../src/Filters.v3 testFilters.v3
+v3c-x86-linux ${VIRGIL_PATH}/lib/util/Vector.v3 ${VIRGIL_PATH}/lib/util/Arrays.v3 ${VIRGIL_PATH}/lib/util/Ranges.v3 ../../src/Pixels.v3 ../../src/Filters.v3 testFilters.v3
